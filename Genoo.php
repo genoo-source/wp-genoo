7cfa023c20c6271bf8c4a557bf9e65147541dd7b
Martin Picha <martin@latorante.name>
2020-11-17 09:05:05 +1000

    Release: 5.8.6

--- Genoo.php
+++ Genoo.php
@@ -5,7 +5,7 @@
     Author:  Genoo, LLC
     Author URI: http://www.genoo.com/
     Author Email: info@genoo.com
-    Version: 5.8.5
+    Version: 5.8.6
     License: GPLv2
     Text Domain: genoo
 */
