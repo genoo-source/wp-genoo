6be12bab9b8e8af922c87aa6e677e4eb3fc2b7f8
GenooBot <admin@genoo.com>
2024-12-01 00:26:41 +0000

    GenooBot: Release 2024-12-01T00:26:41+00:00

--- Genoo.php
+++ Genoo.php
@@ -5,7 +5,7 @@
     Author:  Genoo, LLC
     Author URI: http://www.genoo.com/
     Author Email: info@genoo.com
-    Version: 6.0.17
+    Version: 6.0.18
     License: GPLv2
     Text Domain: genoo
 */
